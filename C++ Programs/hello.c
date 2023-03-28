dc3c88377c0e28d61eee07fe43d56f9f3645ed85
Dheeraj Prasad <117454470+DHEERAJCodes08@users.noreply.github.com>
2023-03-28 14:17:31 +0530

    Delete C++ Programs directory

--- C++ Programs/hello.c
+++
@@ -1,8 +0,0 @@
-/* to mmake the run code in c language we use ./a.exe for C language codes */
-#include<stdio.h>
-
-int main() {
-    printf("Hello World");
-    printf("/n Hello World");
-    return 0;
-}
